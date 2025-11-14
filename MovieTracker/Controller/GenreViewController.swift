4ddbe38c77935f288bc12c10103746005cb3e9b7
Abolfazl Rezaei <abolfazlrezaei.v@gmail.com>
2025-11-14 16:36:25 +0330

    fix padding issue in genre collection

--- MovieTracker/Controller/GenreViewController.swift
+++ MovieTracker/Controller/GenreViewController.swift
@@ -139,7 +139,7 @@ extension GenreViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 10
+        return 0
     }
     
     func collectionView(_ collectionView: UICollectionView,
@@ -152,7 +152,12 @@ extension GenreViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        return UIEdgeInsets(
+            top: 10,
+            left: 10,
+            bottom: 10,
+            right: 10
+        )
     }
 }
 
